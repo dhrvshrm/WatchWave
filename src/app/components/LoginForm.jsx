6a6ee9389dc27fdf4e1cff2c7a35804f955632dc
Dhruv Sharma <shrmadhruv11@gmail.com>
2024-05-29 08:24:43 +0530

    style: login form positioning

--- src/app/components/LoginForm.jsx
+++ src/app/components/LoginForm.jsx
@@ -1,18 +1,24 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, Stack, TextField } from "@mui/material";
 import React from "react";
+
+const STYLES = {
+  loginContainer: {
+    width: "400px",
+    padding: "20px",
+    border: "1px solid black",
+    borderRadius: "10px",
+    backgroundColor: "white",
+    margin: "auto",
+    height: "35rem",
+    mb: "21rem",
+    mt: "6rem",
+    justifyContent: "center",
+  },
+};
 
 function LoginForm() {
   return (
-    <Box
-      sx={{
-        width: "400px",
-        padding: "20px",
-        border: "1px solid black",
-        borderRadius: "10px",
-        backgroundColor: "white",
-        margin: "auto",
-      }}
-    >
+    <Stack sx={STYLES.loginContainer}>
       <TextField label="Email" variant="outlined" fullWidth margin="normal" />
       <TextField
         label="Password"
@@ -23,7 +29,7 @@ function LoginForm() {
       <Button variant="contained" color="primary" fullWidth>
         Login
       </Button>
-    </Box>
+    </Stack>
   );
 }
 
