bdceea016761c985262307b8a4236261c8acf14d
Dhruv <shrmadhruv11@gmail.com>
2024-07-06 23:59:02 +0530

    feat: add user authentication check to RootLayout component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,29 @@
+"use client";
 import { Inter } from "next/font/google";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useUserStore } from "./store/userStore";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
+  const { user } = useUserStore();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user.accessToken === "") {
+      router.push("/login");
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (window.location.pathname === "/") {
+        router.push("/login");
+      }
+    }
+  }, []);
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
