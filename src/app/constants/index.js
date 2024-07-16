a760f1ac1bd26df01275577d3a6e96f50295f9ed
Dhruv <shrmadhruv11@gmail.com>
2024-07-16 21:57:57 +0530

    refactor: update OpenAI configuration to allow browser usage

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -15,4 +15,4 @@ export const TMDB_IMAGE_URL = "https://image.tmdb.org/t/p/w500/";
 
 // Netflix secret
 export const OPEN_AI_KEY =
-  "sk - None - BEOGQ02C1Pe0YQOR5BZuT3BlbkFJDjkHROAnpo432UwZBf4E";
+  "sk-None-l77CaWNT6tYhaqM8zys7T3BlbkFJvQRyPtufDvwOxJK8xulZ";
--- src/app/utils/openai.js
+++ src/app/utils/openai.js
@@ -3,6 +3,7 @@ import { OPEN_AI_KEY } from "../constants";
 
 const openai = new OpenAI({
   apiKey: OPEN_AI_KEY,
+  dangerouslyAllowBrowser: true,
 });
 
 export default openai;
