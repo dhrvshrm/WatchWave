6b0dee5e3cf0b85fd857abe8612edcd6c2147248
Dhruv <shrmadhruv11@gmail.com>
2024-07-17 23:40:55 +0530

    feat: improve search functionality and loading state in GPT search page

--- src/app/gpt-search/page.jsx
+++ src/app/gpt-search/page.jsx
@@ -1,9 +1,17 @@
 "use client";
 
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import dynamic from "next/dynamic";
 import React, { useState } from "react";
 import openai from "../utils/openai";
+import Loader from "../components/Loader";
 
 const STYLES = {
   page: {
@@ -52,17 +60,23 @@ const STYLES = {
     textTransform: "none",
     width: "fit-content",
     backgroundColor: "#1976d2",
+    "&:disabled": {
+      backgroundColor: "rgba(25, 118, 210, 0.5)",
+      color: "white",
+      cursor: "not-allowed",
+    },
     "&:hover": {
       backgroundColor: "#115293",
     },
+    borderRadius: "5px",
   },
 };
 
-function SearchComponent({ setSearchText, handleSearch }) {
+function SearchComponent({ setSearchText, handleSearch, searchText, loading }) {
   return (
     <Stack sx={STYLES.searchContainer} gap={3}>
       <Typography variant="h4" sx={STYLES.title}>
-        Search for your favourite movies here
+        Search for your favourite movie/seriess here
       </Typography>
       <Stack direction="row" justifyContent="center" gap={2}>
         <TextField
@@ -70,15 +84,25 @@ function SearchComponent({ setSearchText, handleSearch }) {
           onChange={(e) => setSearchText(e.target.value)}
           sx={STYLES.textField}
           autoComplete="off"
-          placeholder="Type a movie name..."
+          placeholder="Type a movie/series name..."
         />
         <Button
           variant="contained"
           color="primary"
           sx={STYLES.button}
           onClick={handleSearch}
+          disabled={!searchText || searchText.length < 4}
         >
-          Search
+          {loading ? (
+            <>
+              <Typography variant="body1" sx={{ color: "white" }}>
+                Searching...{" "}
+              </Typography>
+              <CircularProgress size={24} sx={{ ml: 1 }} />
+            </>
+          ) : (
+            "Search"
+          )}
         </Button>
       </Stack>
     </Stack>
@@ -87,6 +111,9 @@ function SearchComponent({ setSearchText, handleSearch }) {
 
 export default function Page() {
   const [searchText, setSearchText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [res, setRes] = useState(null);
+  console.log({ res });
 
   const Header = dynamic(() =>
     import("../components/Header").then((mod) => mod.Header)
@@ -102,12 +129,34 @@ export default function Page() {
 
   const handleSearch = async () => {
     if (searchText) {
+      setLoading(true);
       console.log("searchText", searchText);
+      const gptQuery =
+        'search for movie/seriess like "' +
+        searchText +
+        '"' +
+        " and give me a brief description of the movie/series" +
+        " and give me the release date of the movie/series" +
+        " and give them comma separated" +
+        " and give me the genre of the movie/series" +
+        " and give me the cast of the movie/series" +
+        " and give me the duration of the movie/series" +
+        " and give me the language of the movie/series" +
+        " and give me the trailer of the movie/series" +
+        " and give me the poster of the movie/series" +
+        " and give me the backdrop of the movie/series" +
+        " and give me the tagline of the movie/series" +
+        " and give me the website of the movie/series" +
+        " and give me the imdb rating of the movie/series" +
+        " and need the movie/series details in json format and at least 10 items";
+
+      console.log("gptQuery", gptQuery);
       const chatCompletion = await openai.chat.completions.create({
-        messages: [{ role: "user", content: searchText }],
+        messages: [{ role: "user", content: gptQuery }],
         model: "gpt-3.5-turbo",
       });
-      console.log("chatCompletion", chatCompletion.choices);
+      setRes(JSON.parse(chatCompletion.choices[0].message.content));
+      setLoading(false);
     }
   };
 
@@ -118,6 +167,8 @@ export default function Page() {
         <SearchComponent
           setSearchText={setSearchText}
           handleSearch={handleSearch}
+          searchText={searchText}
+          loading={loading}
         />
         {/* <VignetteWrapper /> */}
         <Footer />
